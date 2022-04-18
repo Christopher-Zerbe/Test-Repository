a116b95d760abac783fad1880627c9424f4c538f
rubbl77 <93820998+rubbl77@users.noreply.github.com>
2022-04-18 18:16:34 +0200

    RubblsBranch

--- main.py
+++ main.py
@@ -2,22 +2,4 @@
 # It is used to test the functionality of the GitHub.
 
 def main():
-    print("Hello World!")
-    print("Hello World!!")
-
-
-def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr) - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
-
-
-test_array = [1, 8, 3, 8, 32, 98, 3, 9, 212, 68, 3, 1]
-
-if __name__ == "__main__":
-    main()
-    print(bubble_sort(test_array))
-
-"Hallo, Ich hab einen PUUUSHHH"
+    print('Hallo Rubbl Branch')
