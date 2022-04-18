a852d8b70232eb451c53b9e37d163d44b7302c51
Christopher Zerbe <christopher@zerbefamily.de>
2022-04-18 16:50:02 +0200

    Added bubble_sort algorithm

--- main.py
+++ main.py
@@ -4,5 +4,17 @@
 def main():
     print("Hello World!")
 
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+test_array = [1, 8, 3, 8, 32, 98, 3, 9, 212, 68, 3, 1]
+
 if __name__ == "__main__":
     main()
+    print(bubble_sort(test_array))
