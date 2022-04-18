04ea40784773fd8f444b770854b974f0c1f3faca
Christopher Zerbe <christopher@zerbefamily.de>
2022-04-18 16:26:37 +0200

    Created simple main.py file.

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is the main file of this test repository.
+# It is used to test the functionality of the GitHub.
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
