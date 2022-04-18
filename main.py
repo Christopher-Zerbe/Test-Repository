bb7b25ac7757ce07f8b6587a648ab5c589351470
Christopher Zerbe <christopher@zerbefamily.de>
2022-04-18 18:17:42 +0200

    Added Current Time

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 # This is the main file of this test repository.
 # It is used to test the functionality of the GitHub.
 
+# import the necessary packages
+import random
+import time
+
+
 def main():
-    print("Hello World!")
-    print("Hello World!!")
-    print("Hello World!")
-    print("Hello World!!")
-    print("Hello World!")
-    print("Hello World!!")
-    print("TestTestTest")
-    print("ich code im Main")
-
-    import time
-    print(time.strftime("%H:%M:%S"))
-
-    
+    print("Hello World! It is currently: " + time.strftime("%H:%M:%S"))
+    print("We recommend the following lotto numbers: " + str(bubble_sort(generate_lotto_numbers())))
 
 
 def bubble_sort(arr):
@@ -24,19 +18,15 @@ def bubble_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
+
 def generate_lotto_numbers():
-    import random
     lotto_numbers = []
     for i in range(6):
         lotto_numbers.append(random.randint(1, 45))
     return lotto_numbers
 
-# Happy Comment
 
 test_array = [1, 8, 3, 8, 32, 98, 3, 9, 212, 68, 3, 1]
 
 if __name__ == "__main__":
     main()
-    print(bubble_sort(test_array))
-    print(generate_lotto_numbers())
-"Hallo, Ich hab einen PUUUSHHH"
