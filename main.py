2c89ffe15ff816e88184aae333cba8eee95bafa1
rubbl77 <93820998+rubbl77@users.noreply.github.com>
2022-04-18 18:32:40 +0200

    TestNr100

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
     print("We recommend the following lotto numbers: " + str(bubble_sort(generate_lotto_numbers())))
     print('rubbls privat branch')
     print('rubbls privat branch')
+    print('rubbls privat branch')
 
 def bubble_sort(arr):
     for i in range(len(arr)):
